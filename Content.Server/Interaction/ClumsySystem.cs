9f3f681ab0bbe75c70cfbe1916c16477f0fce5e4
jessicamaybe <jessica@jessicamaybe.com>
2022-08-03 14:37:30 -0700

    ClumsySystem.cs

---
+++ Content.Server/Interaction/ClumsySystem.cs
@@ -0,0 +1,14 @@
+namespace Content.Server.Interaction
+{
+    /// <summary>
+    /// This handles...
+    /// </summary>
+    public sealed class ClumsySystem : EntitySystem
+    {
+        /// <inheritdoc/>
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+    }
+}
