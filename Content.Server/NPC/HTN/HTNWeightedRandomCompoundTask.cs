7b67e72cd1bf82eca8358195987509923ec9562e
jessicamaybe <jessica@jessicamaybe.com>
2026-04-18 19:55:44 -0700

    comments

--- Content.Server/NPC/HTN/HTNWeightedRandomCompoundTask.cs
+++ Content.Server/NPC/HTN/HTNWeightedRandomCompoundTask.cs
@@ -2,6 +2,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Content.Server.NPC.HTN;
 
+/// <summary>
+/// Selects a HTNCompoundTask based off of random weights
+/// </summary>
 public sealed partial class HTNWeightedRandomCompoundTask : HTNTask
 {
     [DataField(required: true)]
