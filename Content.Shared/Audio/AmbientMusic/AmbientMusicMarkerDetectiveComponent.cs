5d3a4f9807bebd342e11bd58c37657ba4d944922
jessicamaybe <jessica@jessicamaybe.com>
2026-07-18 15:12:38 -0700

    add detective music

---
+++ Content.Shared/Audio/AmbientMusic/AmbientMusicMarkerDetectiveComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Shared.Audio.AmbientMusic;
+
+/// <summary>
+/// Marks an entity as contributing to Detective ambient music
+/// </summary>
+[RegisterComponent]
+public sealed partial class AmbientMusicMarkerDetectiveComponent : Component
+{
+
+}
